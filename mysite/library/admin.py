e545b68a21bb29f333461b671365fb945382b942
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-05-04 20:10:40 +0300

    Initial commit

---
+++ mysite/library/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0  # išjungia placeholder'ius
+    readonly_fields = ('id', )
+    can_delete = False
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'due_back', 'status')
+    list_filter = ('status', 'due_back')
+    search_fields = ('id', 'book__title')
+    list_editable = ('due_back', 'status')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+admin.site.register(BookInstance, BookInstanceAdmin)
